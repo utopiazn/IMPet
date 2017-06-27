7cf50467150a9d26b033d2d4ab094ff15c332c8d
허어녕 <hueonyung>
2017-06-27 21:52:32 +0900

    ajax테스트

--- IMPet/src/main/webapp/WEB-INF/Test/HeoEoNyung.jsp
+++ IMPet/src/main/webapp/WEB-INF/Test/HeoEoNyung.jsp
@@ -1,18 +1,85 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 
 <%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>  
+
 
 <!DOCTYPE html>
 
-
 <html lang="ko">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="<c:url value='/resources/ajax/ajax.js'/>" charset="utf-8"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	//<a href="#this" id="test" >테스트</a> 에서 id부분을 #test로 표시하여 id 클릭시 fn_test 이벤트가 발생함.
+	  $("#test").on("click", function(e){ 
+	       e.preventDefault();
+	       fn_test();
+	   });
+	  
+	//<a href='#this' name='title'>테스트</a> 에서 name이 title인 것을 클릭시 fn_test2가 발생함.
+	  $("a[name='title']").on("click", function(e){ //제목 
+		    e.preventDefault();
+	  		
+	  		// $(this)란 내가 클릭한 곳을 말함.
+		    fn_test2($(this));
+		});
+ 
+});	
+
+// 이 Function 이벤트는 모두 <a 태그로 작성하여 써야함.(input type="button"/ input type="submit")
+function fn_test(){ 
+	//위에 선언해놓은 ajax주소에 function ComSubmit(opt_formId) 부분을 new ComSubmit(); 으로 생성하여 가져다씀.
+      var comSubmit = new ComSubmit();
+	
+	// 내가 보내주고싶은 url
+      comSubmit.setUrl("<c:url value='ajax' />");
+	
+	// 내가 넘겨주고 싶은 값 (글쓰기 같은 경우 값을 넘겨줄 필요가 없으므로 지워도 됌)
+     comSubmit.addParam("no",5);
+	
+	// <form 태그에 submit 버튼 역활을 함.(자세한건 ajax.js에서 보면 알수있음)
+      comSubmit.submit();
+    }
+
+
+function fn_test2(obj){
+	
+	//위에 선언해놓은 ajax주소에 function ComAjax(opt_formId) 부분을 new ComAjax(); 으로 생성하여 가져다씀.
+    var comAjax1 = new ComAjax();
+	
+	//내가 보내고 주고싶은 url 주소
+    comAjax1.setUrl("<c:url value='ajax2' />");
+	
+	// obj.parent() 란 받아온 $(this)가 obj 이며 obj.parent는 내가 클릭한 부분의 부모를 가르키므로 <td class='test'> 부분을 나타낸다.
+	// <td class='test'> td와 /td 사이에 find 함수를 통하여 id가 test, test2인곳을 찾아 value값을 가져온다. </td>
+    comAjax1.addParam("test", obj.parent().find("#test").val());
+    comAjax1.addParam("gab", obj.parent().find("#test2").val());
+    
+    // ajax 기능을 하게 해준다(자세한건 ajax.js에서 보면 알수있음.)
+    comAjax1.ajax1();
+}
+</script>
+
 <head>
 
 	<meta charset="UTF-8">
 <title>허어녕 전용 페이지 </title>
 </head>
-<body>
-
+<body>	
+		<%@ include file="/WEB-INF/ajax/include-body.jspf" %>
+		<a href="#this" id="test" >테스트</a> <!-- 글쓰기 식 -->
+		
+		<table>
+			<tr>
+				<td class='test'>
+					<a href='#this' name='title'>테스트</a> <!-- 글 상세보기 -->
+					<input type='hidden' name='test' id='test' value="string">
+					<input type='hidden' name='test' id='test2' value="${gab }">
+				</td>
+			</tr>
+		</table>
 
 
 </body>
---
+++ IMPet/src/main/webapp/resources/ajax/ajax.js
@@ -0,0 +1,213 @@
+
+//jQuery 에서 Null값 확인 종류 (Null, "Nan", "undefined") 모두 널을 확인한다
+function gfn_isNull(str) {
+	
+	if (str == null) return true;
+	if (str == "NaN") return true;
+	if (new String(str).valueOf() == "undefined") return true;    
+   
+	var chkStr = new String(str);
+    if( chkStr.valueOf() == "undefined" ) return true;
+    if (chkStr == null) return true;    
+    if (chkStr.toString().length == 0 ) return true;   
+    return false; 
+}
+
+function ComSubmit(opt_formId) {
+	// <form 태그 확인 없으면 include 시킨 commonForm으로 지정
+	this.formId = gfn_isNull(opt_formId) == true ? "commonForm" : opt_formId;
+	
+	// 내가 보내줄 URL 주소 
+	this.url = "";
+	
+	// <form 아이디가 commonForm으로 지정됬다면 초기화 시켜줌
+	if(this.formId == "commonForm"){
+		$("#commonForm")[0].reset();
+	}
+	
+	// jsp에서 보내준 URL을 url에다 넣어줌
+	this.setUrl = function setUrl(url){
+		this.url = url;
+	};
+	
+	// 내가 보내줄 값을 모두 히든값으로 넘겨줌 
+	// a 태그에서 값을 넘겨줘도 <form 형식으로 변경하여 넘어가기 때문에 hidden으로 값을 넘겨줌
+	this.addParam = function addParam(key, value){
+		$("#"+this.formId).append($("<input type='hidden' name='"+key+"' id='"+key+"' value='"+value+"' >"));
+	};
+	
+	// submit 버튼 역활을 한다 
+	// 버튼 클릭시 <form name="commandForm", action=url, method="post"> 으로 동작함.
+	this.submit = function submit(){
+		var frm = $("#"+this.formId)[0];
+		frm.action = this.url;
+		frm.method = "post";
+		frm.submit();	
+	};
+	
+	
+}
+
+// 내 자신으로 aJax를 걸기위해 콜백 함수가 필요함.
+var gfv_ajaxCallback = "";
+function ComAjax(opt_formId){
+    this.url = "";    
+    
+    // aJax에서는 필요없지만 최대한 위에것과 변경하지 않기위해 넣어둠.
+   this.formId = gfn_isNull(opt_formId) == true ? "commonForm" : opt_formId;
+    this.param = "";
+     
+    //commonForm으로 됬다면 초기화 시켜주고 body에 넣어줌
+    if(this.formId == "commonForm"){
+        var frm = $("#commonForm");
+        if(frm.length > 0){
+            frm.remove();
+        }
+        var str = "<form id='commonForm' name='commonForm'></form>";
+        $('body').append(str);
+    }
+     
+    // jsp에서 보내준 url 주소
+    this.setUrl = function setUrl(url){
+        this.url = url;
+    };
+     
+    
+    this.setCallback = function setCallback(callBack){
+        gfv_ajaxCallback = callBack;
+    };
+    
+    // url에다가 값을 같이 넘겨주기 위한 로직
+    this.addParam = function addParam(key,value){
+        this.param = this.param + "&" + key + "=" + value;
+    };
+    
+ // List를 뽑을때 자기자신으로 콜백 하기위한 ajax
+    this.ajax = function ajax(){ 
+       if(this.formId != "commonForm"){
+            this.param += "&" + $("#" + this.formId).serialize();
+        }
+        $.ajax({
+            url : this.url,   
+            type : "POST",  
+            data : this.param,
+            success : function(data, status) {
+                if(typeof(gfv_ajaxCallback) == "function"){
+                    gfv_ajaxCallback(data);
+                }
+                else {
+                    eval(gfv_ajaxCallback + "(data);");
+                }
+            }
+        });
+    };
+    
+ // 다른 페이지를 가져오는 ajax
+    this.ajax1 = function ajax1(){ 
+    	alert(this.param);
+        $.ajax({
+            url : this.url,   
+            type : "POST",  
+            data : this.param,
+            async : false,
+            success : function(data) {
+            	$("body").html(data); // 가져온 jsp 를 넣어줄 영역을 표시 <div id="list"> 가져온 jsp <div>
+    		},error: function(data){ // 실패 할경우
+    			alert("실패"); // 실패 alert문구 발생
+    		}   		
+        });
+    };
+}
+
+/*
+divId : 페이징 태그가 그려질 div
+pageIndx : 현재 페이지 위치가 저장될 input 태그 id
+recordCount : 페이지당 레코드 수
+totalCount : 전체 조회 건수
+eventName : 페이징 하단의 숫자 등의 버튼이 클릭되었을 때 호출될 함수 이름
+*/
+var gfv_pageIndex = null;
+var gfv_eventName = null;
+var recordCount = null;
+function gfn_renderPaging(params){
+    var divId = params.divId; //페이징이 그려질 div id
+    gfv_pageIndex = params.pageIndex; //초기값은 1 이되지만 페이지 이동시 jsp 에서 그값을 다시 반환해서 넘겨줌
+    var totalCount = params.totalCount; //총 게시글 수  
+    var currentIndex = $("#"+params.pageIndex).val(); // 초기값 1 제외후 jsp 에서 반환된 값을 넣어줌  
+    if($("#"+params.pageIndex).length == 0 || gfn_isNull(currentIndex) == true){ // 초기 값을 1로 지정해 주는 로직
+        currentIndex = 1;
+    }
+ 
+    recordCount = params.pageRow //페이지당 레코드 수
+    
+ // 페이지의 보여줄 게시글 수를 넘겨받지 않았다면 기본값을 15로 줌.(변경시 jsp에서 comAjax.addParam("PAGE_ROW", 20) 숫자와 맞춰서 변경해야함)
+    if(gfn_isNull(recordCount) == true){ 
+        recordCount = 10; // 
+    }
+    
+ // 전체 게시글 수 / 페에지에서 보여줄 게시글 수를 나눠서 페이지 수를 구해냄.(소수점 자리는 무조건 반올림 한다)
+    var totalIndexCount = Math.ceil(totalCount / recordCount); 
+  
+    gfv_eventName = params.eventName; // jsp 에서 실행될 펑션 함수 fn_selectBoardList 
+     
+    $("#"+divId).empty(); // jsp 에서 div태그로 쓸 PAGE_NAVI 를 초기화 시켜준다.
+    var preStr = ""; // 처음 / 이전 10개페이지 를 담을 함수
+    var postStr = ""; // 마지막 / 다음 10개 페이지를 담을 함수
+    var str = ""; // 이동한 페이지 값을 넣어준다 (예: 25페이지로 이동시 25가 담김)
+     
+    alert(totalCount);
+ // (((1 - 1) / 10) * 10) + 1 = 1;
+ // (((11 - 1) / 10) * 10) + 1 = 11;
+ // (((21 - 1) / 10) * 10) + 1 = 21;
+    var first = (parseInt((currentIndex-1) / 10) * 10) + 1; 
+
+
+    var last = (parseInt(totalIndexCount/10) < parseInt(currentIndex)/10) ? totalIndexCount%10 : 10; 
+    var prev = (parseInt((currentIndex-1)/10)*10) - 9 > 0 ? (parseInt((currentIndex-1)/10)*10) - 9 : 1;
+    var next = (parseInt((currentIndex-1)/10)+1) * 10 + 1 < totalIndexCount ? (parseInt((currentIndex-1)/10)+1) * 10 + 1 : totalIndexCount;
+
+    // 현재 페이지가 1이면 나타나지 않는다.
+    if(currentIndex != 1){
+    	if(totalIndexCount > 10){ //전체 인덱스가 10이 넘을 경우, 맨앞, 앞 태그 작성
+    		preStr += "<a href='#this' class='pad_5' onclick='_movePage(1)'>[<<]</a>" +
+    		"<a href='#this' class='pad_5' onclick='_movePage("+prev+")'>[<]</a>";
+    	}
+    	else if(totalIndexCount <=10 && totalIndexCount > 1){ //전체 인덱스가 10보다 작을경우, 맨앞 태그 작성
+    		preStr += "<a href='#this' class='pad_5' onclick='_movePage(1)'>[<<]</a>";
+    	}
+    }
+    
+    // 현재페이지가 마지막 페이지라면 나타나지 않는다.
+    if(currentIndex != totalIndexCount) {
+    	if(totalIndexCount > 10){ //전체 인덱스가 10이 넘을 경우, 맨뒤, 뒤 태그 작성
+    		postStr += "<a href='#this' class='pad_5' onclick='_movePage("+next+")'>[>]</a>" +
+    		"<a href='#this' class='pad_5' onclick='_movePage("+totalIndexCount+")'>[>>]</a>";
+    	}
+    	else if(totalIndexCount <=10 && totalIndexCount > 1){ //전체 인덱스가 10보다 작을경우, 맨뒤 태그 작성
+    		postStr += "<a href='#this' class='pad_5' onclick='_movePage("+totalIndexCount+")'>[>>]</a>";
+    	}
+    }
+    
+    // 위에서 계산한 first 부터 last 페이지 까지 나타나게 함 
+    // 클릭시 onclick='_movePage 라는 밑에 function으로 이동하게 되면서 페이지이동 하게됨.
+    for(var i=first; i<(first+last); i++){
+        if(i != currentIndex){
+            str += "<a href='#this' class='pad_5' onclick='_movePage("+i+")'>"+i+"</a>";
+        }
+        else{
+            str += "<b><a href='#this' class='pad_5' onclick='_movePage("+i+")'>"+i+"</a></b>";
+        }
+    }
+    $("#"+divId).append(preStr + str + postStr);
+   /* alert(str);*/
+}
+ 
+function _movePage(value){
+    $("#"+gfv_pageIndex).val(value);
+    if(typeof(gfv_eventName) == "function"){
+        gfv_eventName(value);
+    }
+    else {
+        eval(gfv_eventName + "(value);");
+    }
+}
