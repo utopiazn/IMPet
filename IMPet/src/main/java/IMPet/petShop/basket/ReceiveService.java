9dbdc8b321dca61fb2f5a71be1b1e17bde77fbed
김윤아 <anooymik@naver.com>
2017-07-17 15:14:37 +0900

    receiveservice수정

--- IMPet/src/main/java/IMPet/petShop/basket/ReceiveService.java
+++ IMPet/src/main/java/IMPet/petShop/basket/ReceiveService.java
@@ -1,10 +1,11 @@
 package IMPet.petShop.basket;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ReceiveService {
 	
-	Map<String, Object> selectAll(Map<String, Object> map) throws Exception;
+	List<Map<String, Object>> selectAll(Map<String, Object> map) throws Exception;
 	
 	Map<String, Object> selectOne(Map<String, Object> map) throws Exception;
 	
--- IMPet/src/main/java/IMPet/petShop/basket/ReceiveServiceImpl.java
+++ IMPet/src/main/java/IMPet/petShop/basket/ReceiveServiceImpl.java
@@ -1,34 +1,40 @@
 package IMPet.petShop.basket;
 
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 @Service(value="receiveService")
 public class ReceiveServiceImpl implements ReceiveService {
+	
+	@Resource(name="receiveDAO")
+	private ReceiveDAO receiveDAO; 
 
 	@Override
-	public Map<String, Object> selectAll(Map<String, Object> map) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Map<String, Object>> selectAll(Map<String, Object> map) throws Exception {
+		
+		return receiveDAO.selectAll(map);
 	}
 
 	@Override
 	public Map<String, Object> selectOne(Map<String, Object> map) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return receiveDAO.selectOne(map);
 	}
 
 	@Override
 	public void insert(Map<String, Object> map) throws Exception {
-		// TODO Auto-generated method stub
-		
+
+		receiveDAO.insert(map);
 	}
 
 	@Override
 	public void update(Map<String, Object> map) throws Exception {
-		// TODO Auto-generated method stub
 		
+		receiveDAO.update(map);
 	}
 
 }
