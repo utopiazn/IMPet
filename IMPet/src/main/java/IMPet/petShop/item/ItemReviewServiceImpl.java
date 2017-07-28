f01c721a26fdcfe1b213fc3a8e6bfffb0cd57666
김윤아 <anooymik@naver.com>
2017-07-28 13:52:07 +0900

    펫샵정리

--- IMPet/src/main/java/IMPet/petShop/item/ItemReviewServiceImpl.java
+++ IMPet/src/main/java/IMPet/petShop/item/ItemReviewServiceImpl.java
@@ -13,29 +13,34 @@ public class ItemReviewServiceImpl implements ItemReviewService{
 	@Resource(name="itemReviewDAO")
 	private ItemReviewDAO itemReviewDAO;
 	
+	//후기상세보기
 	@Override
 	public Map<String, Object> selectOne(Map<String, Object> map) throws Exception {
 		
 		return itemReviewDAO.selectOne(map);
 	}
 
+	//후기삭제
 	@Override
 	public void delete(Map<String, Object> map) throws Exception {
 
 		itemReviewDAO.delete(map);
 	}
 	
+	//후기수정
 	@Override
 	public void update(Map<String, Object> map) throws Exception {
 
 		itemReviewDAO.update(map);
 	}
 	
+	//후기등록
 	@Override
 	public void insert(Map<String, Object> map) throws Exception {
 		itemReviewDAO.insert(map);
 	}
 
+	//후기리스트
 	@Override
 	public List<Map<String, Object>> selectList(Map<String, Object> map) throws Exception {
 	
